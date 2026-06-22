c0e206605567bfa25d22aee448dd0c3bd6b384db
falrux <142692930+yeenrux@users.noreply.github.com>
2026-06-21 19:04:44 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { next } from '@vercel/edge';
 
-export default function middleware(req) {gtefucked
+export default function middleware(req) {
   return next({
     headers: {
       'Referrer-Policy': 'origin-when-cross-origin',
